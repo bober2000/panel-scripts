590345cd8a650f1e975f0e58168ba046e210a164
Administrator <info@rolique.io>
2016-06-14 22:53:28 +0300

    Add create_domain

---
+++ create_domain.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require 'config.php';
+
+$parser = new PEAR2\Console\CommandLine(array(
+    'description' => 'Creates new domain',
+    'version'     => '0.0.1', // the version of your program
+));
+$parser->addOption(
+   'interactive',
+   array(
+      'short_name'  => '-i',
+      'long_name'   => '--interactive',
+      'description' => 'Run interactively',
+      'action'      => 'StoreTrue'
+  )
+);
+$parser->addOption(
+   'name',
+   array(
+       'short_name'  => '-n',
+       'long_name'   => '--name',
+       'description' => 'domain name',
+       'action'      => 'StoreString',
+       'help_name'   => 'domain_name'
+  )
+);
+$parser->addOption(
+   'owner',
+   array(
+       'short_name'  => '-o',
+       'long_name'   => '--owner',
+       'description' => 'system user name',
+       'action'      => 'StoreString',
+       'help_name'   => 'system_user_name'
+  )
+);
+$parser->addOption(
+   'address',
+   array(
+       'short_name'  => '-a',
+       'long_name'   => '--ip-address',
+       'description' => 'domain IP address',
+       'action'      => 'StoreString',
+       'help_name'   => 'ip_address'
+  )
+);
+
+try {
+   $result = $parser->parse();
+   if ($result->options["interactive"]){
+      //TODO Запуск в интерактивном режиме
+   }
+   if (empty(array_filter($result->options))){
+      $parser->displayUsage();
+   }
+   try {
+     $cmd = escapeshellcmd('id -u ' . $result->options["owner"]);
+     $uid = exec($cmd . " 2>&1",$aResult, $return_val);
+     if ($return_val<>0)
+        throw new \Exception ("Can't get user uid");
+   } catch (Exception $ex) {
+        echo 'Error: ',  $ex->getMessage(), "\n";
+        exit(1);
+   }
+   try {
+     $cmd = escapeshellcmd('id -g ' . $result->options["owner"]);
+     $gid = exec($cmd . " 2>&1",$aResult, $return_val);
+     if ($return_val<>0)
+        throw new \Exception ("Can't get user gid");
+   } catch (Exception $ex) {
+        echo 'Error: ',  $ex->getMessage(), "\n";
+        exit(1);
+   }
+
+   try {
+     if(! @mkdir('/home/' . $result->options["owner"] . '/domains/' . $result->options["name"], 0755)) {
+       $mkdirErrorArray = error_get_last();
+       throw new Exception('Cant create directory ' .$mkdirErrorArray['message'], 1);
+     }
+   } catch (Exception $ex) {
+        echo 'Error: ',  $ex->getMessage(), "\n";
+        exit(1);
+   }
+}
+catch (Exception $exc) {
+   $parser->displayError($exc->getMessage());
+}
